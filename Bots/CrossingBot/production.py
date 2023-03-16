db2d494dd6ea9ed5ed578c84f9ffeb7dc4f60c55
Ethan Zimbra <ethan.zimbra@gmail.com>
2023-03-16 14:38:33 -0600

    Trialing new position close logic

--- Bots/CrossingBot/production.py
+++ Bots/CrossingBot/production.py
@@ -116,14 +116,14 @@ def multiProduction():
    ip = "127.0.0.1"
    # WBD ##########
    connectionWBD = ibapi.ConnectionInfo(ip, 7497, 1)
-   wbdBot = CrossingBot(60, 100, 0.3, 2.0, 36, 25, 24, log_file_name="CrossingProduction.txt", name="ProdWBDBot", log_level=logging.INFO, simulation=False)
+   wbdBot = CrossingBot(60, 100, 0.3, 1.0, 36, 25, 8, 0.2 log_file_name="CrossingProduction.txt", name="ProdWBDBot", log_level=logging.INFO, simulation=False)
    wbdAPI = ibapi.IBInterface(wbdBot, Contract(symbol="WBD"))
    startBot(connectionWBD, wbdBot, wbdAPI)
    time.sleep(10)
 
    # META ##########
    connectionMETA = ibapi.ConnectionInfo(ip, 7497, 2)
-   metaBot = CrossingBot(40, 60, 0.3, 2.0, 8, 15, 12, log_file_name="CrossingProduction.txt", name="ProdMETABot", log_level=logging.INFO, simulation=False)
+   metaBot = CrossingBot(40, 100, 0.3, 3.0, 8, 35, 12, 0.1, log_file_name="CrossingProduction.txt", name="ProdMETABot", log_level=logging.INFO, simulation=False)
    metaAPI = ibapi.IBInterface(metaBot, Contract(symbol="META"))
    startBot(connectionMETA, metaBot, metaAPI)
 
--- Bots/CrossingBot/simulation.py
+++ Bots/CrossingBot/simulation.py
@@ -11,6 +11,7 @@ def build_trader_list(frame="short"):
       rsi_list = [4, 8, 12]
       purge_list = [5, 10, 15]
       sell_rsi_list = [4, 8, 12]
+      sell_crossing_list = [0.1]
    elif frame == "fast":
       average_window_list = [9, 12]
       crossing_window_list = [5, 8]
@@ -18,16 +19,18 @@ def build_trader_list(frame="short"):
       stop_loss_list = [1.0, 2.0]
       rsi_list = [4, 8]
       purge_list = [10, 15]
-      sell_rsi_list = [8, 12]
+      sell_rsi_list = [8]
+      sell_crossing_list = [-0.1, 0.1, 0.4, 0.7]
    elif frame == "mixed":
       average_window_list = [9, 12, 20, 40, 60, 80]
       crossing_window_list = [5, 8, 10]
-      crossing_threshold_list = [.1, .2, .3]
+      crossing_threshold_list = [.2, .3]
       stop_loss_list = [1.0, 1.5, 2.0, 2.5]
       rsi_list = [4, 8, 12]
       purge_list = [5, 10, 15]
-      sell_rsi_list = [4, 8, 12]
-   else:
+      sell_rsi_list = [8, 12]
+      sell_crossing_list = [-0.1, 0.1, 0.2]
+   elif frame == "long":
       average_window_list = [40, 60, 80]
       crossing_window_list = [40, 60, 80, 100]
       crossing_threshold_list = [.3]
@@ -35,6 +38,7 @@ def build_trader_list(frame="short"):
       rsi_list = [8, 12, 24, 36]
       purge_list = [10, 15, 25, 35]
       sell_rsi_list = [8, 12, 24]
+      sell_crossing_list = [0.1, 0.2]
 
    for average_window in average_window_list:
       for crossing_window in crossing_window_list:
@@ -43,20 +47,25 @@ def build_trader_list(frame="short"):
                for rsi in rsi_list:
                   for purge in purge_list:
                      for sell_rsi in sell_rsi_list:
-                        if rsi <= average_window + crossing_window and sell_rsi <= average_window + crossing_window:
-                           bot = CrossingBot(average_window, crossing_window, crossing_threshold, stop_loss, rsi, purge, sell_rsi)
-                           bot.trading_enabled = True
-                           trader_list.append(bot)
+                        for sell_crossing in sell_crossing_list:
+                            if rsi <= average_window + crossing_window and sell_rsi <= average_window + crossing_window:
+                                bot = CrossingBot(average_window, crossing_window, crossing_threshold, stop_loss, rsi, purge, sell_rsi, sell_crossing)
+                                bot.trading_enabled = True
+                                trader_list.append(bot)
 
    return trader_list
 
 
 def simSingle():
    print("Simulating Single")
-   trader = CrossingBot(9, 5, 0.2, 1.0, 4, 10, 12, simulation=True)
-   simulate_single_trader(trader, "C:\\Users\\ezimb\\source\\repos\\IBBotTransfer\\IBBot\\Data\\SP500_10Min\\Tickers\\EPAM\\EPAM_2023-02-28_10 mins.csv")
-   #trader = CrossingBot(12, 8, 0.3, 1.0, 4, 15, 8, simulation=True)
-   #simulate_single_trader(trader, "C:\\Users\\ezimb\\source\\repos\\IBBotTransfer\\IBBot\\Data\\SP500_10Min\\Tickers\\META\\META_2023-02-28_10 mins.csv")
+   # trader = CrossingBot(9, 5, 0.2, 1.0, 4, 10, 12, simulation=True)
+   # simulate_single_trader(trader, "C:\\Users\\ezimb\\source\\repos\\IBBotTransfer\\IBBot\\Data\\SP500_10Min\\Tickers\\EPAM\\EPAM_2023-02-28_10 mins.csv")
+   
+   # trader = CrossingBot(40, 60, 0.3, 1.0, 8, 15, 12, simulation=True)
+   # simulate_single_trader(trader, "C:\\Users\\ezimb\\source\\repos\\IBBotTransfer\\IBBot\\Data\\SP500_10Min\\Tickers\\META\\META_2023-02-28_10 mins.csv")
+   for crossing in [-1.0, -.4, -.3, -.2, -.1, 0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0]:
+      trader = CrossingBot(40, 60, 0.3, 1.0, 8, 15, 12, crossing, simulation=True)
+      simulate_single_trader(trader, "C:\\Users\\ezimb\\source\\repos\\IBBotTransfer\\IBBot\\Data\\SP500_10Min\\Tickers\\META\\META_2023-02-28_10 mins.csv")
 
 
 def simSP500():
