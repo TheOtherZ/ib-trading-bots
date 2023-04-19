2122caaba51f4d5cc1a6f4fb8c29235efddd737d
Ethan Zimbra <ethan.zimbra@gmail.com>
2023-04-19 12:56:45 -0600

    Production changes

--- Bots/CrossingBot/production.py
+++ Bots/CrossingBot/production.py
@@ -45,44 +45,67 @@ def startBot(connection_info, bot, symbol):
 
 def multiProduction():
    CapitalManager.initialize(41000)
+
+   # print("waiting for market open")
+   # time.sleep(36000)
    ip = "127.0.0.1"
+   api_list = []
    connection = 1
    # WBD ##########
    connectionWBD = ConnectionInfo(ip, 7497, connection)
    wbdBot = CrossingBot(40, 60, 0.3, 2.0, 8, 25, 24, 0.3, False, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdWBDBot", log_level=logging.INFO, simulation=False)
    wbdAPI = startBot(connectionWBD, wbdBot, "WBD")
+   api_list.append(wbdAPI)
 
    # META ##########
    connection += 1
    connectionMETA = ConnectionInfo(ip, 7497, connection)
    metaBot = CrossingBot(60, 80, 0.3, 3.0, 8, 15, 12, -0.1, False, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdMETABot", log_level=logging.INFO, simulation=False)
    metaAPI= startBot(connectionMETA, metaBot, "META")
+   api_list.append(metaAPI)
 
    # CCL ##########
    connection += 1
    connectionCCL = ConnectionInfo(ip, 7497, 3)
    cclBot = CrossingBot(40, 100, 0.3, 2.5, 8, 10, 8, 0.3, False, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdCCLBot", log_level=logging.INFO, simulation=False)
    cclAPI = startBot(connectionCCL, cclBot, "CCL")
+   api_list.append(cclAPI)
 
    # EPAM ##########
    connection += 1
    connectionEPAM = ConnectionInfo(ip, 7497, connection)
    epamBot = CrossingBot(9, 5, 0.2, 1.0, 4, 10, 12, -1, True, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdEPAMBot", log_level=logging.INFO, simulation=False)
    epamAPI = startBot(connectionEPAM, epamBot, "EPAM")
+   api_list.append(epamAPI)
 
    # MRNA ##########
    connection += 1
    connectionMRNA = ConnectionInfo(ip, 7497, connection)
    mrnaBot = CrossingBot(12, 10, 0.2, 2.0, 4, 15, 8, -0.1, False, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdMRNABot", log_level=logging.INFO, simulation=False)
    mrnaAPI = startBot(connectionMRNA, mrnaBot, "MRNA")
+   api_list.append(mrnaAPI)
 
    # F ##########
    connection += 1
    connectionF = ConnectionInfo(ip, 7497, connection)
    fBot = CrossingBot(9, 5, 0.1, 2.5, 12, 10, 12, -1, True, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdFBot", log_level=logging.INFO, simulation=False)
    fAPI = startBot(connectionF, fBot, "F")
+   api_list.append(fAPI)
 
-   api_list = [wbdAPI, metaAPI, cclAPI, epamAPI, mrnaAPI, fAPI]
+   # DHI ##########
+   connection += 1
+   connectionDHI = ConnectionInfo(ip, 7497, connection)
+   dhiBot = CrossingBot(20, 10, 0.1, 1.5, 8, 5, 8, 0.3, True, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdDHIBot", log_level=logging.INFO, simulation=False)
+   dhiAPI = startBot(connectionDHI, dhiBot, "DHI")
+   api_list.append(dhiAPI)
+
+   # FANG ##########
+   connection += 1
+   connectionFANG = ConnectionInfo(ip, 7497, connection)
+   fangBot = CrossingBot(20, 8, 0.1, 2.5, 8, 5, 12, 0.3, True, capital=10000.0, log_file_name="CrossingProduction.txt", name="ProdFANGBot", log_level=logging.INFO, simulation=False)
+   fangAPI = startBot(connectionFANG, fangBot, "FANG")
+   api_list.append(fangAPI)
+
 
    # Handle multi-day?
    while True:
